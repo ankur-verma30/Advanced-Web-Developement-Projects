06540babfc85778fe905f0b457f2cad7a790ba61
Ankur Verma <ankurverma7619@gmail.com>
2024-08-05 13:49:37 +0530

    Adding Click Events

--- shopping-cart/main.js
+++ shopping-cart/main.js
@@ -1,14 +1,16 @@
 import './style.css'
 import shopData from './data.js'
 
-
 let shop = document.getElementById('shop')
 
+
+
 let generateShop = () => {
+  
 return(shop.innerHTML=shopData.map((x)=>{     //map function 
     let {id,name,price,desc,img}=x           //destructuring of object
 return `
-<div class="item">
+<div  id=product-id-${id} class="item">
       <img width="220" src=${img} alt="">
       <div class="details">
         <h3>${name}</h3>
@@ -16,9 +18,9 @@ return `
         <div class="price-quantity">
           <h2>$${price}</h2>
           <div class="buttons">
-            <i class="bi bi-dash-lg"></i>
-            <div class="quantity">0</div>
-            <i class="bi bi-plus-lg"></i>
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id=${id} class="quantity">0</div>
+            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
           </div>
         </div>
       </div>
@@ -28,3 +30,20 @@ return `
 }
 
 generateShop();
+
+let basket=[
+  {}
+]
+
+window.increment=(id)=>{
+  let selectedItem=
+  console.log('Increment',id);
+  }
+  
+  window.decrement=(id)=>{
+    console.log('Decrement',id);
+  }
+  
+  window.updateQuantity=()=>{}
+  
+
